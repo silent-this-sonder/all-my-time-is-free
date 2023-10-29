c62e5f5d7bac219c30730ea43cca60af994ff3ef
silent-this-sonder <143755460+silent-this-sonder@users.noreply.github.com>
2023-10-28 22:25:27 -0400

    Add input validation with date validation

---
+++ tmods/inputver.py
@@ -0,0 +1,30 @@
+"""Verifies user input."""
+
+import re
+
+def __date_regex(a):
+    date = re.compile(r"""(\d?\d) # month
+                      [-/] # separator (- or /)
+                      (\d?\d) # day
+                      [-/] # separator (- or /)
+                      (\d\d\d\d)""", # year
+                      re.VERBOSE)
+    match = date.search(a)
+    return match
+    
+def input_date(prompt: str, err=""):
+    """Get user input and make sure that it is a date.
+        prompt - question to display to user
+        err - optional, what to display if the input is invalid"""
+    
+    while True:
+        answer = input(prompt)
+        match = __date_regex(answer)
+
+        if match:
+            month = match[1]
+            day = match[2]
+            year = match[3]
+            return month, day, year
+        
+        print(err)
