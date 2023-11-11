2ac61a06d8eb70f2662770f2bc5ae12de0d685af
silent-this-sonder <143755460+silent-this-sonder@users.noreply.github.com>
2023-11-11 17:06:06 -0500

    Add creation date to the taskdict for tasks.

--- tmods/tasktrack.py
+++ tmods/tasktrack.py
@@ -1,19 +1,19 @@
 import datetime
 
-def create_taskdict(title: str, duedate: datetime.date, desc="", project=""):
+
+def create_taskdict(title: str, creation: str, duedate: str, desc="", project=""):
     """Create a task in the form of a dictionary.
         title - str, short name for the task
-        duedate - datetime.date instance that tells when it is due
+        duedate - MM-DD-YYYY format that tells when it is due
         desc - str, longer description of the task
         project - str, project/school subject the task is for"""
     
     task = {
         "title": title,
-        "duedate": str(duedate),
+        "creation": creation,
+        "duedate": duedate,
         "desc": desc,
         "project": project
     }
 
     return task
-
-t = create_taskdict("Hello World", datetime.date.today())
